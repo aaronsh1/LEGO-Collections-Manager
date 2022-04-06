858235b81ed8346ad980d881410b55b379ea3e17
brentonStroberg <brentons@bbd.co.za>
2022-04-06 22:19:02 +0200

    initail commit

---
+++ LegoCollectionManager/LegoCollectionManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LegoCollectionManager.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}

3be781cfeced801b1c41cdcdbee9aeba273bcd91
aaronsh <aaronsh@bbd.co.za>
2022-04-08 12:23:29 +0200

    Finished the CRUD operations for SetController (must still be tested when data is inputed into database).

--- LegoCollectionManager/LegoCollectionManager/Controllers/SetController.cs
+++ LegoCollectionManager/LegoCollectionManager/Controllers/SetController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using LegoCollectionManager.Models;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace LegoCollectionManager.Controllers
 {
@@ -18,23 +20,27 @@ namespace LegoCollectionManager.Controllers
             if (userId == null)
                 return NotFound();
 
-            List<int> cats = (from s in _context.Sets
-                              join sc in _context.UserSets on sc.UserSetId equals s.SetId
+            List<int?> categories = (from s in _context.Sets
+                              join sc in _context.UserSets on s.SetId equals sc.UseSetId 
                               where sc.User == userId
-                              select new
-                              {
-                                  Category = s.Category,
-                              }).ToList();
+                              select s.SetCategory).ToList();
 
             List<Set> recomSets = (from s in _context.Sets
-                              where cats.contains(s.Category)
+                              where categories.Contains(s.SetCategory)
                               select s).ToList();
+
+            return View(recomSets);
         }
 
         // GET: SetController/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(int? id)
         {
-            return View();
+            if (id == null)
+                return NotFound();
+
+            Set setToReturn = new Set();
+            setToReturn = _context.Sets.Find(id);
+            return View(setToReturn);
         }
 
         // GET: SetController/Create
@@ -46,8 +52,11 @@ namespace LegoCollectionManager.Controllers
         // POST: SetController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Set setToAdd)
         {
+            _context.Sets.Add(setToAdd);
+            _context.SaveChanges();
+
             try
             {
                 return RedirectToAction(nameof(Index));
@@ -59,37 +68,63 @@ namespace LegoCollectionManager.Controllers
         }
 
         // GET: SetController/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int? id)
         {
-            return View();
+            if (id == null)
+                return NotFound();
+
+            Set setToEdit = (from s in _context.Sets
+                             where s.SetId == id
+                             select s).ToList().FirstOrDefault();
+            return View(setToEdit);
         }
 
         // POST: SetController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(Set setToEdit)
         {
+            Set originalSet = (from s in _context.Sets
+                             where s.SetId == setToEdit.SetId
+                             select s).ToList().FirstOrDefault();
+
+            if (originalSet == null)
+                return NotFound();
+
+            _context.Entry(originalSet).CurrentValues.SetValues(setToEdit);
+            _context.SaveChanges();
             try
             {
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction($"Details/{setToEdit.SetId}");
             }
             catch
             {
-                return View();
+                return View(nameof(Index));
             }
         }
 
         // GET: SetController/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(int? id)
         {
+            if (id == null)
+                return NotFound();
+
+            Set SetToDelete = (from s in _context.Sets
+                             where s.SetId == id
+                             select s).ToList().FirstOrDefault();
             return View();
         }
 
         // POST: SetController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(Set setToDelete)
         {
+            if (setToDelete == null)
+                return NotFound();
+
+            _context.Remove(setToDelete);
+            _context.SaveChanges();
             try
             {
                 return RedirectToAction(nameof(Index));

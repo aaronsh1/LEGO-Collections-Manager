742325dfeb748a2f5941262e2a4282f31136727c
aaronsh <aaronsh@bbd.co.za>
2022-04-11 22:32:28 +0200

    Added vews for the Set controller and added the relevant logic in the controller.

--- LegoCollectionManager/LegoCollectionManager/Controllers/SetController.cs
+++ LegoCollectionManager/LegoCollectionManager/Controllers/SetController.cs
@@ -1,14 +1,37 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Web;
 using LegoCollectionManager.Models;
 using System.Linq;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 
 namespace LegoCollectionManager.Controllers
 {
     public class SetController : Controller
     {
         LegoCollectionDBContext _context = new LegoCollectionDBContext();
+
+        public IEnumerable<SelectListItem> getSetCategories()
+        {
+            IEnumerable<SelectListItem> setCategories = from sc in _context.SetCategories
+                                                        select new SelectListItem
+                                                        {
+                                                            Text = sc.SetCategoryName,
+                                                            Value = sc.SetCategoryId.ToString()
+                                                        };
+            return setCategories;
+        }
+
+        public IEnumerable<SetPiece> getSetPieces(int id)
+        {
+            IEnumerable<SetPiece> setPieces = from sc in _context.SetPieces
+                                              where sc.SetId == id
+                                              select sc;
+
+            return setPieces;
+        }
         // GET: SetController
         public ActionResult Index()
         {
@@ -54,6 +77,8 @@ namespace LegoCollectionManager.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(Set setToAdd)
         {
+            ViewBag["SetCategory"] = getSetCategories();
+
             _context.Sets.Add(setToAdd);
             _context.SaveChanges();
 
@@ -65,6 +90,35 @@ namespace LegoCollectionManager.Controllers
             {
                 return View();
             }
+        }
+
+        public ActionResult AddPieces(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            ViewBag.SetId = 100;
+            IEnumerable<SetPiece> pieces = (from sp in _context.SetPieces
+                                            where sp.SetId == id
+                                            select sp);
+
+
+            return View(pieces);
+        }
+
+        [HttpPost]
+        public ActionResult AddPieces(int setId, FormCollection form)
+        {
+            SetPiece SetPieceToAdd = new SetPiece();
+            SetPieceToAdd.Piece = Int32.Parse(form["Piece"]);
+            SetPieceToAdd.SetId = setId;
+            SetPieceToAdd.Amount = Int32.Parse(form["Amount"]);
+            SetPieceToAdd.Colour = Int32.Parse(form["Colour"]);
+
+            _context.SetPieces.Add(SetPieceToAdd);
+            _context.SaveChanges();
+
+            return View(setId);
         }
 
         // GET: SetController/Edit/5

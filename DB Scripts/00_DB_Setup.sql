4b698105faee1b2a2ec57dbcb1280abe9910abb7
aaronsh <aaronsh@bbd.co.za>
2022-04-20 10:02:43 +0200

    Updated the DB script to include DELETE CASCADE for various foreign keys and took out the substitution tables.

--- DB Scripts/00_DB_Setup.sql
+++ DB Scripts/00_DB_Setup.sql
@@ -13,26 +13,10 @@ GO
 CREATE TABLE [Piece] (
   [PieceId] varchar(50) PRIMARY KEY,
   [PieceName] varchar(255),
-  [PieceCategory] int
-)
-GO
+  [PieceCategory] int,
 
-CREATE TABLE [SubstitutePool] (
-  [SubstitutePool] int PRIMARY KEY IDENTITY(1, 1),
-  [Name] varchar(255)
-)
-GO
-
-CREATE TABLE [SubstitutePoolItem] (
-  [SubstitutePoolItemId] int PRIMARY KEY IDENTITY(1, 1),
-  [Piece] varchar(50),
-  [SubstitutePool] int
-)
-GO
-
-CREATE TABLE [UserSubsititutePool] (
-  [User] int,
-  [SubstitutePool] int
+  ADD FOREIGN KEY ([PieceCategory]) REFERENCES [PieceCategory] ([PieceCategoryId])
+  ON DELETE CASCADE
 )
 GO
 
@@ -41,7 +25,13 @@ CREATE TABLE [SetPiece] (
   [Piece] varchar(50),
   [SetId] int,
   [Amount] int,
-  [Colour] int
+  [Colour] int,
+
+  ADD FOREIGN KEY ([Piece]) REFERENCES [Piece] ([PieceId])
+  ON DELETE CASCADE,
+  ADD FOREIGN KEY ([SetId]) REFERENCES [Set] ([SetId])
+  ON DELETE CASCADE,
+  ADD FOREIGN KEY ([Colour]) REFERENCES [Colour] ([ColourId])
 )
 GO
 
@@ -55,7 +45,10 @@ CREATE TABLE [Set] (
   [SetId] int PRIMARY KEY,
   [SetName] varchar(255),
   [PiecesAmount] int,
-  [SetCategory] int
+  [SetCategory] int,
+
+  ADD FOREIGN KEY ([SetCategory]) REFERENCES [SetCategory] ([SetCategoryId])
+  ON DELETE CASCADE
 )
 GO
 
@@ -75,14 +68,25 @@ CREATE TABLE [UserSparePieces] (
   [User] int,
   [Piece] varchar(50),
   [Amount] int,
-  [Colour] int
+  [Colour] int,
+
+  ADD FOREIGN KEY ([User]) REFERENCES [User] ([UserId])
+  ON DELETE CASCADE,
+  ADD FOREIGN KEY ([Piece]) REFERENCES [Piece] ([PieceId])
+  ON DELETE CASCADE,
+  ADD FOREIGN KEY ([Colour]) REFERENCES [Colour] ([ColourId])
 )
 GO
 
 CREATE TABLE [UserSet] (
   [UseSetId] int PRIMARY KEY IDENTITY(1, 1),
   [User] int,
-  [Set] int
+  [Set] int,
+
+  ADD FOREIGN KEY ([User]) REFERENCES [User] ([UserId])
+  ON DELETE CASCADE,
+  ADD FOREIGN KEY ([Set]) REFERENCES [Set] ([SetId])
+  ON DELETE CASCADE
 )
 GO
 
@@ -97,70 +101,24 @@ CREATE TABLE [MissingPiece] (
   [Piece] varchar(50),
   [UserSet] int,
   [Amount] int,
-  [Colour] int
+  [Colour] int,
+
+  ADD FOREIGN KEY ([UserSet]) REFERENCES [UserSet] ([UseSetId])
+  ON DELETE CASCADE,
+  ADD FOREIGN KEY ([Piece]) REFERENCES [Piece] ([PieceId])
+  ON DELETE CASCADE,
+  ADD FOREIGN KEY ([Colour]) REFERENCES [Colour] ([ColourId])
 )
 GO
 
 CREATE TABLE [SetPieceCategory] (
   [SetPieceCategoryId] int PRIMARY KEY IDENTITY(1, 1),
   [PieceCategory] int,
-  [Set] int
+  [Set] int,
+
+  ADD FOREIGN KEY ([PieceCategory]) REFERENCES [PieceCategory] ([PieceCategoryId])
+  ON DELETE CASCADE,
+  ADD FOREIGN KEY ([Set]) REFERENCES [Set] ([SetId])
+  ON DELETE CASCADE
 )
-GO
-
-ALTER TABLE [Set] ADD FOREIGN KEY ([SetCategory]) REFERENCES [SetCategory] ([SetCategoryId])
-GO
-
-ALTER TABLE [Piece] ADD FOREIGN KEY ([PieceCategory]) REFERENCES [PieceCategory] ([PieceCategoryId])
-GO
-
-ALTER TABLE [SetPiece] ADD FOREIGN KEY ([Piece]) REFERENCES [Piece] ([PieceId])
-GO
-
-ALTER TABLE [SetPiece] ADD FOREIGN KEY ([SetId]) REFERENCES [Set] ([SetId])
-GO
-
-ALTER TABLE [UserSparePieces] ADD FOREIGN KEY ([User]) REFERENCES [User] ([UserId])
-GO
-
-ALTER TABLE [UserSparePieces] ADD FOREIGN KEY ([Piece]) REFERENCES [Piece] ([PieceId])
-GO
-
-ALTER TABLE [UserSet] ADD FOREIGN KEY ([User]) REFERENCES [User] ([UserId])
-GO
-
-ALTER TABLE [UserSet] ADD FOREIGN KEY ([Set]) REFERENCES [Set] ([SetId])
-GO
-
-ALTER TABLE [SetPieceCategory] ADD FOREIGN KEY ([PieceCategory]) REFERENCES [PieceCategory] ([PieceCategoryId])
-GO
-
-ALTER TABLE [SetPieceCategory] ADD FOREIGN KEY ([Set]) REFERENCES [Set] ([SetId])
-GO
-
-ALTER TABLE [MissingPiece] ADD FOREIGN KEY ([UserSet]) REFERENCES [UserSet] ([UseSetId])
-GO
-
-ALTER TABLE [MissingPiece] ADD FOREIGN KEY ([Piece]) REFERENCES [Piece] ([PieceId])
-GO
-
-ALTER TABLE [SetPiece] ADD FOREIGN KEY ([Colour]) REFERENCES [Colour] ([ColourId])
-GO
-
-ALTER TABLE [UserSparePieces] ADD FOREIGN KEY ([Colour]) REFERENCES [Colour] ([ColourId])
-GO
-
-ALTER TABLE [MissingPiece] ADD FOREIGN KEY ([Colour]) REFERENCES [Colour] ([ColourId])
-GO
-
-ALTER TABLE [SubstitutePoolItem] ADD FOREIGN KEY ([SubstitutePool]) REFERENCES [SubstitutePool] ([SubstitutePool])
-GO
-
-ALTER TABLE [SubstitutePoolItem] ADD FOREIGN KEY ([Piece]) REFERENCES [Piece] ([PieceId])
-GO
-
-ALTER TABLE [UserSubsititutePool] ADD FOREIGN KEY ([SubstitutePool]) REFERENCES [SubstitutePool] ([SubstitutePool])
-GO
-
-ALTER TABLE [UserSubsititutePool] ADD FOREIGN KEY ([User]) REFERENCES [User] ([UserId])
 GO
